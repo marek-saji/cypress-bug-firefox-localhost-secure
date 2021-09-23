89a72bad6ba5757e815df54e09058682a5c23fe5
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2021-09-23 13:03:55 +0200

    Failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('window.isSecureContext', () => {
+  it('runs on firefox', () => {
+    expect(window.navigator.userAgent).to.include('Firefox')
+  })
+  it('runs on localhost', () => {
+    expect(window.location.hostname).to.equal('localhost')
+  })
+  it('is true', () => {
+    expect(window.isSecureContext).to.be.true;
   })
 })
